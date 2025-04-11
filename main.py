8c1e93652b5d6118889b777638e5dafdfe077a10
iMac1 <irina.macovei@yahoo.fr>
2025-04-11 16:19:41 +0300

    sprint

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 class Human:
-    def __init__(self, pos_x, pos_y, speed):
-        self.__pos_x = pos_x
-        self.__pos_y = pos_y
-        self.__speed = speed
+    def sprint_right(self):
+        pass
+
+    def sprint_left(self):
+        pass
+
+    def sprint_up(self):
+        pass
+
+    def sprint_down(self):
+        pass
+
+    def __raise_if_cannot_sprint(self):
+        pass
+
+    def __use_sprint_stamina(self):
+        pass
+
+    # don't touch below this line
 
     def move_right(self):
         self.__pos_x += self.__speed
@@ -17,4 +32,10 @@ class Human:
         self.__pos_y -= self.__speed
 
     def get_position(self):
-        return (self.__pos_x, self.__pos_y)
+        return self.__pos_x, self.__pos_y
+
+    def __init__(self, pos_x, pos_y, speed, stamina):
+        self.__pos_x = pos_x
+        self.__pos_y = pos_y
+        self.__speed = speed
+        self.__stamina = stamina
