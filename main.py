8671d38b168ee78d73439f19dd5d4b54a147a5ef
iMac1 <irina.macovei@yahoo.fr>
2025-04-12 12:18:16 +0300

    the files thst should have been after merging the pull request

--- main.py
+++ main.py
@@ -1,41 +1,42 @@
-class Human:
-    def sprint_right(self):
-        pass
+import random
 
-    def sprint_left(self):
-        pass
+class DeckOfCards:
+    SUITS = ["Hearts", "Diamonds", "Clubs", "Spades"]
+    RANKS = [
+        "Ace",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
+        "Jack",
+        "Queen",
+        "King",
+    ]
 
-    def sprint_up(self):
-        pass
+    def __init__(self):
+        self.__cards = []
+        self.create_deck()
 
-    def sprint_down(self):
-        pass
+    def create_deck(self):
+        for suit in self.SUITS:
+            for rank in self.RANKS:
+                self.__cards.append((rank, suit))
+                
 
-    def __raise_if_cannot_sprint(self):
-        pass
+    def shuffle_deck(self):
+        random.shuffle(self.__cards)
 
-    def __use_sprint_stamina(self):
-        pass
+    def deal_card(self):
+        if self.__cards:
+            return self.__cards.pop()
+        return None
 
     # don't touch below this line
 
-    def move_right(self):
-        self.__pos_x += self.__speed
-
-    def move_left(self):
-        self.__pos_x -= self.__speed
-
-    def move_up(self):
-        self.__pos_y += self.__speed
-
-    def move_down(self):
-        self.__pos_y -= self.__speed
-
-    def get_position(self):
-        return self.__pos_x, self.__pos_y
-
-    def __init__(self, pos_x, pos_y, speed, stamina):
-        self.__pos_x = pos_x
-        self.__pos_y = pos_y
-        self.__speed = speed
-        self.__stamina = stamina
+    def __str__(self):
+        return f"The deck has {len(self.__cards)} cards"
