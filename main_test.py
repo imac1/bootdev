615bb2c4658a47c6667564da62ce219c1695f5f6
kitsune <irina.macovei@yahoo.fr>
2025-04-20 04:02:09 +0300

    adition magic method (#14)

--- main_test.py
+++ main_test.py
@@ -1,36 +1,40 @@
 from main import *
 
 run_cases = [
-    (Dragon("Green Dragon", -1, -2, 1, 2, 1), -2, -3, 0, 0, True),
-    (Dragon("Red Dragon", 2, 2, 2, 2, 2), 0, 1, 1, 0, True),
-    (Dragon("Gold Dragon", 0, 0, 5, 5, 10), 4, 0, 5, 1, False),
-    (Dragon("Gold Dragon", 0, 0, 20, 20, 20), 10, 10, 20, 20, True),
+    (Sword("bronze"), Sword("bronze"), "iron", None),
+    (Sword("bronze"), Sword("iron"), None, "cannot craft"),
 ]
 
 submit_cases = run_cases + [
-    (Dragon("Blue Dragon", 4, -3, 2, 1, 1), 0, 0, 10, 10, False),
-    (Dragon("Black Dragon", 5, -1, 3, 2, 2), -10, -10, 10, 10, True),
+    (Sword("steel"), Sword("steel"), None, "cannot craft"),
+    (Sword("iron"), Sword("iron"), "steel", None),
+    (Sword("bronze"), Sword("steel"), None, "cannot craft"),
 ]
 
 
-def test(dragon, input1, input2, input3, input4, expected_output):
-    print("---------------------------------")
-    print(f" * Dragon pos_x: {dragon.pos_x}")
-    print(f" * Dragon pos_y: {dragon.pos_y}")
-    print(f" * Dragon height: {dragon.height}")
-    print(f" * Dragon width: {dragon.width}")
-    print("")
-    print(f" * Area x1: {input1}")
-    print(f" * Area y1: {input2}")
-    print(f" * Area x2: {input3}")
-    print(f" * Area y2: {input4}")
-    print("")
-    result = dragon.in_area(input1, input2, input3, input4)
-    print(f"Expected in area: {expected_output}")
-    print(f"Actual in area:   {result}")
-    if result == expected_output:
-        return True
-    return False
+def test(sword1, sword2, expected_result, expected_err):
+    try:
+        print("---------------------------------")
+        print(f"{sword1.sword_type} sword + {sword2.sword_type} sword...")
+        result = sword1 + sword2
+
+        if expected_err:
+            print(f"Expected Exception: {expected_err}")
+            print("Actual Exception:    None")
+            return False
+
+        print(f"Expected: {expected_result}")
+        print(f"Actual:   {result.sword_type}")
+        if result.sword_type != expected_result:
+            return False
+
+    except Exception as e:
+        print(f"Expected Exception: {expected_err}")
+        print(f"Actual Exception:   {e}")
+        if expected_err != str(e):
+            return False
+
+    return True
 
 
 def main():
@@ -40,10 +44,8 @@ def main():
     for test_case in test_cases:
         correct = test(*test_case)
         if correct:
-            print("Pass")
             passed += 1
         else:
-            print("Fail")
             failed += 1
     if failed == 0:
         print("============= PASS ==============")
