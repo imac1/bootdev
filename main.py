8ce8e7f0066334bc275195175f110ef9a1dd6e6a
kitsune <irina.macovei@yahoo.fr>
2025-04-20 11:42:23 +0300

    Revert "Cards" (#16)
    * Revert "suits and rankes indexes"
    
    This reverts commit e7e672c42815895484ce1e0e448daf9e73417b3b.
    
    * Revert "define rank and suit as instance variables"
    
    This reverts commit 7051c1cf95aae31619d261b963c920a3a84c8f37.

--- main.py
+++ main.py
@@ -5,10 +5,7 @@ RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", 
 
 class Card:
     def __init__(self, rank, suit):
-        self.__rank = rank
-        self.__suit = suit
-        self.rank_index = RANKS.index(rank)
-        self.suit_index = SUITS.index(suit)
+        pass
 
     def __eq__(self, other):
         pass
