280313cb2a5f0b1b1cceabac9b86534d4dd4ec72
iMac1 <irina.macovei@yahoo.fr>
2025-04-27 22:50:04 +0300

    low card fction

--- main.py
+++ main.py
@@ -50,6 +50,17 @@ class HighCardRound(Round):
             return 0
         
 
-
 class LowCardRound(Round):
-    pass
+
+    def __init__(self, card1, card2):
+        self.card1 = card1
+        self.card2 = card2
+
+    def resolve_round(self):
+        if self.card1 < self.card2:
+            return 1
+        elif self.card1 > self.card2:
+            return 2
+        else:
+            return 0
+        
